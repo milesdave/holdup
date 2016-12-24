ebc3ad678bcef1d64ba335987edbcd471bdd7e12
Miles <davis.g.miles@gmail.com>
2016-12-24 19:47:10 +1030

    fixed bug with stringTrunc()

--- holdup.h
+++ holdup.h
@@ -1,14 +1,13 @@
 #ifndef _HOLDUP_H
 #define _HOLDUP_H
 
-#include <stdbool.h>
 #include "types.h"
 
 info_t info;
 
 void process(FTSENT *node);
 void copy(const char *src, const char *dest);
-void indent(int);
+void indent(int i);
 
 int byType(const FTSENT **a, const FTSENT **b);
 
