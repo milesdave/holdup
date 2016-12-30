1fccd8cf5231b711afa5fad2ad4de0e526b92c64
Miles Davis <davis.g.miles@gmail.com>
2016-12-30 13:37:32 +1030

    patched new output from master

--- holdup.h
+++ holdup.h
@@ -8,6 +8,7 @@ info_t info;
 void process(FTSENT *node);
 void copy(const char *src, const char *dest);
 void indent(int i);
+void printSize(const size_t size);
 
 int byType(const FTSENT *const *a, const FTSENT *const *b);
 
